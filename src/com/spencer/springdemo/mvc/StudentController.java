e7f07b80af634345d0ebb67e3dddaa07866dc6c2
5pence <sbarriball@me.com>
2019-04-24 13:22:12 +0100

    add processForm method to StudentController

--- src/com/spencer/springdemo/mvc/StudentController.java
+++ src/com/spencer/springdemo/mvc/StudentController.java
@@ -2,6 +2,7 @@ package com.spencer.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +19,13 @@ public class StudentController {
         model.addAttribute("student", student);
 
         return "student-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+
+        // log the input data
+        System.out.println("student: " + student.getFirstName() + " " + student.getLastName());
+        return "student-confirmation";
     }
 }
